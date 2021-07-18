548fbc98b4667e7e13c5543a29acba432e3f3b88
ori-bar <ori.bar.emet@gmail.com>
2021-07-18 17:23:30 +0300

    CR-2526 : added role definition to azure environment context + implemented no_custom_subscription_owner_roles_exist rule

--- cloudrail/knowledge/context/azure/azure_environment_context.py
+++ cloudrail/knowledge/context/azure/azure_environment_context.py
@@ -1,6 +1,7 @@
 from typing import List, Dict
 
 from cloudrail.knowledge.context.aliases_dict import AliasesDict
+from cloudrail.knowledge.context.azure.azure_role_definition import AzureRoleDefinition
 from cloudrail.knowledge.context.azure.security.azure_security_center_auto_provisioning import AzureSecurityCenterAutoProvisioning
 from cloudrail.knowledge.context.azure.security.azure_security_center_contact import AzureSecurityCenterContact
 from cloudrail.knowledge.context.azure.network.azure_vnet_gateway import AzureVirtualNetworkGateway
@@ -35,7 +36,8 @@ class AzureEnvironmentContext(BaseEnvironmentContext):
                  function_apps: AliasesDict[AzureFunctionApp] = None,
                  vnet_gateways: AliasesDict[AzureVirtualNetworkGateway] = None,
                  security_center_contacts: AliasesDict[AzureSecurityCenterContact] = None,
-                 security_center_auto_provisioning: AliasesDict[AzureSecurityCenterAutoProvisioning] = None):
+                 security_center_auto_provisioning: AliasesDict[AzureSecurityCenterAutoProvisioning] = None,
+                 role_definitions: List[AzureRoleDefinition] = None):
         BaseEnvironmentContext.__init__(self)
         self.checkov_results: Dict[str, List[CheckovResult]] = checkov_results or {}
         self.resource_groups: AliasesDict[AzureResourceGroup] = resource_groups or AliasesDict()
@@ -51,3 +53,4 @@ class AzureEnvironmentContext(BaseEnvironmentContext):
         self.security_center_auto_provisioning: AliasesDict[AzureSecurityCenterAutoProvisioning] = security_center_auto_provisioning or AliasesDict()
         self.security_center_contacts: AliasesDict[AzureSecurityCenterContact] = security_center_contacts or AliasesDict()
         self.vnet_gateways: AliasesDict[AzureVirtualNetworkGateway] = vnet_gateways or AliasesDict()
+        self.role_definitions = role_definitions or []
