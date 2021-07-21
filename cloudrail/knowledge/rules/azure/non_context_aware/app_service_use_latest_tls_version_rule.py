ce41e80fed982eac5f3faca21ba5fc35b57912d5
Alon Ashkenazi <alon@indeni.com>
2021-07-21 17:17:02 +0300

    fix alert evidence string

--- cloudrail/knowledge/rules/azure/non_context_aware/app_service_use_latest_tls_version_rule.py
+++ cloudrail/knowledge/rules/azure/non_context_aware/app_service_use_latest_tls_version_rule.py
@@ -14,13 +14,14 @@ class AppServiceUseLatestTlsVersionRule(AzureBaseRule):
     def execute(self, env_context: AzureEnvironmentContext, parameters: Dict[ParameterType, any]) -> List[Issue]:
         issues: List[Issue] = []
         for app_service in env_context.app_services:
-            if app_service.app_service_config is not None and \
-                    (tls_version := version.parse(app_service.app_service_config.minimum_tls_version) < version.parse('1.2')):
-                issues.append(
-                    Issue(
-                        f'The Web App `{app_service.get_friendly_name()}` uses `{tls_version}` for '
-                        f'the minimum TLS version, instead of 1.2.', app_service, app_service))
-        return issues
+            if app_service.app_service_config is not None:
+                tls_version = version.parse(app_service.app_service_config.minimum_tls_version)
+                if tls_version < version.parse('1.2'):
+                    issues.append(
+                        Issue(
+                            f'The Web App `{app_service.get_friendly_name()}` uses `{tls_version}` for '
+                            f'the minimum TLS version, instead of 1.2.', app_service, app_service))
+            return issues
 
     def should_run_rule(self, environment_context: AzureEnvironmentContext) -> bool:
         return bool(environment_context.app_services)
