c51737ffbee126856a9d60b2fb46230e73b7a7d7
Lir Shindelman <lirshindelman@Lirs-MacBook-Pro.local>
2021-08-08 16:44:56 +0300

    CR-2263-non_car_diagnostic_logs_enabled_in_app_services-change to data class

--- cloudrail/knowledge/context/azure/webapp/diagnostic_logs.py
+++ cloudrail/knowledge/context/azure/webapp/diagnostic_logs.py
@@ -1,8 +1,9 @@
+from dataclasses import dataclass
 
+
+@dataclass
 class DiagnosticLogs:
-
-    def __init__(self, detailed_error_logging_enabled: bool, http_logging_enabled: bool, request_tracing_enabled: bool) -> None:
-        self.detailed_error_logging_enabled: bool = detailed_error_logging_enabled
-        self.http_logging_enabled: bool = http_logging_enabled
-        self.request_tracing_enabled: bool = request_tracing_enabled
+    detailed_error_logging_enabled: bool
+    http_logging_enabled: bool
+    request_tracing_enabled: bool
 
