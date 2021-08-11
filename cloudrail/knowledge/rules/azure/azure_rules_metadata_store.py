363f60967e5e17cb7db27a7d2816afde0732e2ac
ori-bar <ori.bar.emet@gmail.com>
2021-08-11 16:14:36 +0300

    CR-2968: refactor metadata store

---
+++ cloudrail/knowledge/rules/azure/azure_rules_metadata_store.py
@@ -0,0 +1,9 @@
+import os
+
+from cloudrail.knowledge.rules.rules_metadata_store import RulesMetadataStore, read_metadata_file
+
+
+class AzureRulesMetadataStore(RulesMetadataStore):
+    def __init__(self):
+        raw_data = read_metadata_file(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'azure_rules_metadata.yaml'))
+        super().__init__(raw_data)
