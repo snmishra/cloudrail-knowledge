ad054abcf50ab4b88619b0bb77fac36fb6b8b23d
lirshindelman <lirshindelman@gmail.com>
2021-08-15 17:20:10 +0300

    CR-2267-add new line

--- tests/knowledge/rules/azure/non_context_aware/test_app_service_non_car_client_certificates_required_in_web_app_rule.py
+++ tests/knowledge/rules/azure/non_context_aware/test_app_service_non_car_client_certificates_required_in_web_app_rule.py
@@ -38,3 +38,4 @@ class TestAppServiceClientCertificatesRequiredRule(unittest.TestCase):
             self.assertEqual(1, len(result.issues))
         else:
             self.assertEqual(RuleResultType.SUCCESS, result.status)
+
