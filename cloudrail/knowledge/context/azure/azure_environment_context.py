857f8ae4d7bc6348090371bc98a7f70d0899ae77
lirshindelman <lirshindelman@gmail.com>
2021-08-26 16:36:23 +0300

    CR-2270-not-car-azure-ensure-function-app-use-latest-java-ver2

--- cloudrail/knowledge/context/azure/azure_environment_context.py
+++ cloudrail/knowledge/context/azure/azure_environment_context.py
@@ -1,3 +1,4 @@
+from cloudrail.knowledge.context.azure.vmss.azure_virtual_machine_scale_set import AzureVirtualMachineScaleSet
 from typing import Dict, List
 
 from cloudrail.knowledge.context.aliases_dict import AliasesDict
@@ -30,7 +31,6 @@ from cloudrail.knowledge.context.azure.vm.azure_virtual_machine import AzureVirt
 from cloudrail.knowledge.context.azure.webapp.azure_app_service import AzureAppService
 from cloudrail.knowledge.context.azure.webapp.azure_app_service_config import AzureAppServiceConfig
 from cloudrail.knowledge.context.azure.webapp.azure_function_app import AzureFunctionApp
-from cloudrail.knowledge.context.azure.webapp.azurerm_app_service_plan import AzureAppServicePlan
 from cloudrail.knowledge.context.base_environment_context import (BaseEnvironmentContext, CheckovResult)
 
 
@@ -66,7 +66,8 @@ class AzureEnvironmentContext(BaseEnvironmentContext):
                  public_ips: AliasesDict[AzurePublicIp] = None,
                  network_security_group_rules: List[AzureNetworkSecurityRule] = None,
                  app_security_groups: AliasesDict[AzureApplicationSecurityGroup] = None,
-                 nic_application_security_group_association: AliasesDict[AzureNetworkInterfaceApplicationSecurityGroupAssociation] = None
+                 nic_application_security_group_association: AliasesDict[AzureNetworkInterfaceApplicationSecurityGroupAssociation] = None,
+                 virtual_machines_scale_sets: AliasesDict[AzureVirtualMachineScaleSet] = None
                  ):
         BaseEnvironmentContext.__init__(self)
         self.checkov_results: Dict[str, List[CheckovResult]] = checkov_results or {}
@@ -101,4 +102,4 @@ class AzureEnvironmentContext(BaseEnvironmentContext):
         self.app_security_groups: AliasesDict[AzureApplicationSecurityGroup] = app_security_groups or AliasesDict()
         self.network_interface_application_security_group_association: AliasesDict[AzureNetworkInterfaceApplicationSecurityGroupAssociation] = \
             nic_application_security_group_association or AliasesDict()
-        self.app_service_plans: AliasesDict[AzureAppServicePlan] = app_service_plans or AliasesDict()
+        self.virtual_machines_scale_sets: AliasesDict[AzureVirtualMachineScaleSet] = virtual_machines_scale_sets or AliasesDict()
