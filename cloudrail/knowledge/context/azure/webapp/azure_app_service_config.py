f57fe208d88a689c6f38ec32f397131219d174e2
lirshindelman <lirshindelman@gmail.com>
2021-08-29 13:22:56 +0300

    CR-2270-only linux_fx_version

--- cloudrail/knowledge/context/azure/webapp/azure_app_service_config.py
+++ cloudrail/knowledge/context/azure/webapp/azure_app_service_config.py
@@ -31,7 +31,6 @@ class AzureAppServiceConfig(AzureResource):
         self.http2_enabled: bool = http2_enabled
         self.logs: DiagnosticLogs = logs
         self.linux_fx_version: str = linux_fx_version
-        self.java_version: str = java_version
 
     def get_keys(self) -> List[str]:
         return [self.get_name()]
