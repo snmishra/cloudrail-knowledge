4e533f259f9a36f29d45984ef337777f022bfcd1
LirShindalman <49649760+lirshindalman@users.noreply.github.com>
2021-08-31 10:42:27 +0300

    Cr 2270 not car azure ensure function app use latest java ver (#175)
    * Modified basic builders for rule
    
    * Added metadata rule
    
    * azure-CR-2270-not-car-azure-ensure-function-app-use-latest-java-ver
    
    * azure-CR-2270
    
    * CR-2270
    
    * CR-2270-add language_type
    
    * CR-2270
    
    * CR-2270
    
    * CR-2270-not-car-azure-ensure-function-app-use-latest-java-ver
    
    * CR-2270-not-car-azure-ensure-function-app-use-latest-java-ver
    
    * CR-2270-not-car-azure-ensure-function-app-use-latest-java-ver
    
    * CR-2270-not-car-azure-ensure-function-app-use-latest-java-ver
    
    * CR-2270-not-car-azure-ensure-function-app-use-latest-java-ver2
    
    * CR-2270-not-car-azure-ensure-function-app-use-latest-java-ver3
    
    * CR-2270
    
    * CR-2270
    
    * CR-2270
    
    * CR-2270
    
    * CR-2270
    
    * CR-2270
    
    * CR-2270
    
    * CR-2270-
    
    * CR-2270-
    
    * CR-2270-
    
    * CR-2270-
    
    * CR-2270-only linux_fx_version
    
    * CR-2270- add java_version and linux_fx_version
    
    * CR-2270-Documentation
    
    * Merge branch 'main' into CR-2270-not-car-azure-ensure-function-app-use-latest-java-ver
    
    * CR-2270-metadata
    
    * CR-2270-metadata
    
    * CR-2270-metadata
    
    * CR-2270-metadata
    
    * CR-2270-metadata
    
    * CR-2270
    
    * CR-2270
    
    Co-authored-by: Imanuel <imanuel.m@indeni.com>

--- cloudrail/knowledge/rules/rule_metadata.py
+++ cloudrail/knowledge/rules/rule_metadata.py
@@ -37,6 +37,7 @@ class SecurityLayer(str, Enum):
     DISASTER_RECOVERY = 'disaster_recovery'
     STORAGE = 'storage'
     TAGGING = 'tagging'
+    RUN_TIME = 'runtime'
 
 
 class ResourceType(str, Enum):
