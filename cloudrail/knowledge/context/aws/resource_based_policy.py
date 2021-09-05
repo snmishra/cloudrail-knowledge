1bfc0f47f4814b63fb1e50bd6775c96551aa2fca
lirshindelman <lirshindelman@gmail.com>
2021-09-05 16:13:51 +0300

    CR-3134-add  ResourceBasedPolicy

--- cloudrail/knowledge/context/aws/resource_based_policy.py
+++ cloudrail/knowledge/context/aws/resource_based_policy.py
@@ -1,18 +1,13 @@
+from abc import abstractmethod
 from typing import List
-from cloudrail.knowledge.context.aws.iam.policy import Policy, PolicyType
-from cloudrail.knowledge.context.aws.iam.policy_statement import PolicyStatement
-from cloudrail.knowledge.context.aws.service_name import AwsServiceName
+from cloudrail.knowledge.context.aws.iam.policy import Policy
 
 
 class ResourceBasedPolicy(Policy):
 
-    def __init__(self,
-                 account: str,
-                 statements: List[PolicyStatement],
-                 raw_document: str = None,
-                 aws_service_name: AwsServiceName = AwsServiceName.AWS_IAM_POLICY,
-                 policy_type: PolicyType = PolicyType.RESOURCE_POLICY):
-        super().__init__(account, statements, raw_document, aws_service_name, policy_type)
+    @abstractmethod
+    def get_keys(self) -> List[str]:
+        pass
 
     @staticmethod
     def is_standalone() -> bool:
