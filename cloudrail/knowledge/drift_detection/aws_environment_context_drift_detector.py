9a22e3f562f17bc074d18b91b15e59d8a708eb67
Imanuel <imanuel.m@indeni.com>
2021-09-14 14:05:58 +0300

    CR-3058: modified drift functions

--- cloudrail/knowledge/drift_detection/aws_environment_context_drift_detector.py
+++ cloudrail/knowledge/drift_detection/aws_environment_context_drift_detector.py
@@ -1,3 +1,15 @@
+from cloudrail.knowledge.context.aws.resources.athena.athena_database import AthenaDatabase
+from cloudrail.knowledge.context.aws.resources.cloudhsmv2.cloudhsm_v2_cluster import CloudHsmV2Cluster
+from cloudrail.knowledge.context.aws.resources.cloudhsmv2.cloudhsm_v2_hsm import CloudHsmV2Hsm
+from cloudrail.knowledge.context.aws.resources.ec2.transit_gateway_route_table_propagation import TransitGatewayRouteTablePropagation
+from cloudrail.knowledge.context.aws.resources.ec2.vpc_endpoint_route_table_association import VpcEndpointRouteTableAssociation
+from cloudrail.knowledge.context.aws.resources.glue.glue_connection import GlueConnection
+from cloudrail.knowledge.context.aws.resources.iam.iam_group_membership import IamGroupMembership
+from cloudrail.knowledge.context.aws.resources.iam.iam_policy_attachment import IamPolicyAttachment
+from cloudrail.knowledge.context.aws.resources.s3.s3_bucket_object import S3BucketObject
+from cloudrail.knowledge.context.aws.resources.s3outposts.s3outpost_endpoint import S3OutpostEndpoint
+from cloudrail.knowledge.context.aws.resources.worklink.worklink_fleet import WorkLinkFleet
+from cloudrail.knowledge.context.aws.resources.aws_resource import AwsResource
 from typing import Set
 
 from cloudrail.knowledge.drift_detection.base_environment_context_drift_detector import BaseEnvironmentContextDriftDetector
@@ -56,3 +68,10 @@ class AwsEnvironmentContextDriftDetector(BaseEnvironmentContextDriftDetector):
                 'qualified_arn',
                 'owner_id',
                 'owner_name'}
+
+    @classmethod
+    def supported_aws_drift_resources(cls, new: AwsResource) -> bool:
+        return not (isinstance(new, (AthenaDatabase, GlueConnection, WorkLinkFleet,
+                                     S3OutpostEndpoint, CloudHsmV2Hsm, CloudHsmV2Cluster,
+                                     S3BucketObject, TransitGatewayRouteTablePropagation, IamGroupMembership,
+                                     VpcEndpointRouteTableAssociation, IamPolicyAttachment)))
