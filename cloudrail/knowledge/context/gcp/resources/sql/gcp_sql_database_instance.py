3c8190a9b8127fb9891a70b87f90343ff737421e
Doron Afota <doronafota@Dorons-MacBook-Pro.local>
2021-10-13 15:18:49 +0300

    first push

--- cloudrail/knowledge/context/gcp/resources/sql/gcp_sql_database_instance.py
+++ cloudrail/knowledge/context/gcp/resources/sql/gcp_sql_database_instance.py
@@ -1,20 +1,284 @@
 from typing import List, Optional
+from enum import Enum
+from dataclasses import dataclass
+from datetime import datetime
 
 from cloudrail.knowledge.context.gcp.resources.constants.gcp_resource_type import GcpResourceType
 from cloudrail.knowledge.context.gcp.resources.gcp_resource import GcpResource
 
 
+class GcpSqlDBInstanceVersion(Enum):
+    MYSQL56 = 'MYSQL_5_6'
+    MYSQL57 = 'MYSQL_5_7'
+    MYSQL80 = 'MYSQL_8_0'
+    POSTGRES96 = 'POSTGRES_9_6'
+    POSTGRES10 = 'POSTGRES_10'
+    POSTGRES11 = 'POSTGRES_11'
+    POSTGRES12 = 'POSTGRES_12'
+    POSTGRES13 = 'POSTGRES_13'
+    SQLSERVER_2017_STANDARD = 'SQLSERVER_2017_STANDARD'
+    SQLSERVER_2017_ENTERPRISE = 'SQLSERVER_2017_ENTERPRISE'
+    SQLSERVER_2017_EXPRESS = 'SQLSERVER_2017_EXPRESS'
+    SQLSERVER_2017_WEB = 'SQLSERVER_2017_WEB'
+    SQLSERVER_2019_STANDARD = 'SQLSERVER_2019_STANDARD'
+    SQLSERVER_2019_ENTERPRISE = 'SQLSERVER_2019_ENTERPRISE'
+    SQLSERVER_2019_EXPRESS = 'SQLSERVER_2019_EXPRESS'
+    SQLSERVER_2019_WEB = 'SQLSERVER_2019_WEB'
+
+@dataclass
+class GcpSqlDBInstanceSettingsDBFlags:
+    """
+        Attributes:
+            name: (Required) Name of the flag.
+            value: (Required) Value of the flag.
+    """
+    name: str
+    value: str
+
+
+@dataclass
+class GcpSqlDBInstanceSettingsBackupRetention:
+    """
+        Attributes:
+            retained_backups: (Required) Depending on the value of retention_unit, this is used to determine if a backup needs to be deleted.
+            retention_unit: (Optional) The unit that 'retained_backups' represents. Defaults to COUNT.
+    """
+    retained_backups: int
+    retention_unit: Optional[str]
+
+
+@dataclass
+class GcpSqlDBInstanceSettingsBackupConfig:
+    """
+        Attributes:
+            binary_log_enabled: (Optional) True if binary logging is enabled. Applicable for MySql only.
+            enabled: (Optional) True if backup configuration is enabled.
+            start_time: (Optional) HH:MM format time indicating when backup configuration starts.
+            point_in_time_recovery_enabled: (Optional) True if Point-in-time recovery is enabled. Applicable for PostGres only.
+            location: (Optional) The region where the backup will be stored.
+            transaction_log_retention_days: (Optional) The number of days of transaction logs retained for point in time restore, from 1-7.
+            backup_retention_settings: (Optional) A subblock of backup retention configuration parameters.
+    """
+    binary_log_enabled: Optional[bool]
+    enabled: Optional[bool]
+    start_time: Optional[datetime]
+    point_in_time_recovery_enabled: Optional[bool]
+    location: Optional[str]
+    transaction_log_retention_days: Optional[int]
+    backup_retention_settings: Optional[GcpSqlDBInstanceSettingsBackupRetention]
+
+
+@dataclass
+class GcpSqlDBInstanceIPConfigAuthNetworks:
+    """
+        Attributes:
+            expiration_time: (Optional) The RFC 3339 formatted date time string indicating when this whitelist expires.
+            name: (Optional) A name for this whitelist entry.
+            value: (Required) A CIDR notation IPv4 or IPv6 address that is allowed to access this instance. Must be set even if other two attributes are not for the whitelist to become active.
+    """
+    expiration_time: Optional[datetime]
+    name: Optional[str]
+    value: str
+
+
+@dataclass
+class GcpSqlDBInstanceSettingsIPConfig:
+    """
+        Attributes:
+            ipv4_enabled: (Optional) If the Sql instance should be assigned a public ipv4 address. Either ipv4_enabled must be enabled or a private_network must be configured.
+            private_network: (Optional) The VPC network from which the Cloud SQL instance is accessible for private IP.
+            require_ssl: (Optional) An indication if this instance requires SSL or not.
+            authorized_networks: (Optional) A sublist that specifies networks authorized to access the resources.
+    """
+    ipv4_enabled: Optional[bool]
+    private_network: Optional[str]
+    require_ssl: Optional[bool]
+    authorized_networks: Optional[List[GcpSqlDBInstanceIPConfigAuthNetworks]]
+
+
+@dataclass
+class GcpSqlDBInstanceSettingsLocPref:
+    """
+        Attributes:
+            follow_gae_application: (Optional) A GAE application whose zone to remain in. Must be in the same region as this instance.
+            zone: (Optional) The preferred compute engine zone.
+    """
+    follow_gae_application: Optional[str]
+    zone: Optional[str]
+
+
+@dataclass
+class GcpSqlDBInstanceSettingsMaintWindow:
+    """
+        Attributes:
+            day: (Optional) Day of week (1-7), starting on Monday.
+            hour: (Optional) Hour of day (0-23), ignored if day not set.
+            update_track: (Optional) Receive updates earlier (canary) or later (stable).
+    """
+    day: Optional[int]
+    hour: Optional[int]
+    update_track: Optional[str]
+
+
+@dataclass
+class GcpSqlDBInstanceSettingsInsights:
+    """
+        Attributes:
+            query_insights_enabled: (Optional) True if Query Insights feature is enabled.
+            query_string_length: (Optional) Maximum query length stored in bytes. Between 256 and 4500. Default to 1024.
+            record_application_tags: (Optional) True if Query Insights will record application tags from query when enabled.
+            record_client_address: (Optional) True if Query Insights will record client address when enabled.
+    """
+    query_insights_enabled: Optional[bool]
+    query_string_length: Optional[int]
+    record_application_tags: Optional[bool]
+    record_client_address: Optional[bool]
+
+
+@dataclass
+class GcpSqlDBInstanceSettings:
+    """
+        Attributes:
+            tier: (Required) The machine type to use.
+            activation_policy: (Optional) This specifies when the instance should be active.
+            authorized_gae_applications: (Optional, Deprecated) This property is only applicable to First Generation instances.
+            availability_type: (Optional) The availability type of the Cloud SQL instance, high availability (REGIONAL) or single zone (ZONAL).
+            collation: (Optional) The name of server instance collation.
+            crash_safe_replication: (Optional, Deprecated) (Applicable to First Generation instances) Specific to read instances, indicates when crash-safe replication flags are enabled.
+            disk_autoresize: (Optional, Default: true) Configuration to increase storage size automatically.
+            disk_size: (Optional, Default: 10) The size of data disk, in GB.
+            disk_type: (Optional, Default: PD_SSD) The type of data disk. Available options are PD_SSD and PD_HDD.
+            pricing_plan: (Optional) Pricing plan for this instance, can only be PER_USE.
+            replication_type: (Optional, Deprecated) This property is only applicable to First Generation instances. Replication type for this instance, can be one of ASYNCHRONOUS or SYNCHRONOUS.
+            user_labels: (Optional) A set of key/value user label pairs to assign to the instance.
+            database_flags: (Optional) A sublist to support database flags. A "name" and "value" are required.
+            backup_configuration: (Optional) A subblock of backup configuration parameters.
+            ip_configuration: (Optional) A sublock of IP configuration parameters.
+            location_preference: (Optional) A subblock of location preference parameters.
+            maintenance_window: (Optional) A subblock of maintenance window parameters.
+            insights_config: (Optional) A subblock for instances to declare Query Insights configuration.
+    """
+    tier: str
+    activation_policy: Optional[int]
+    authorized_gae_applications: Optional[List[str]]
+    availability_type: Optional[str]
+    collation: Optional[str]
+    crash_safe_replication: Optional[bool]
+    disk_autoresize: Optional[bool]
+    disk_size: Optional[int]
+    disk_type: Optional[str]
+    pricing_plan: Optional[str]
+    replication_type: Optional[str]
+    user_labels: Optional[dict]
+    database_flags: Optional[List[GcpSqlDBInstanceSettingsDBFlags]]
+    backup_configuration: Optional[GcpSqlDBInstanceSettingsBackupConfig]
+    ip_configuration: Optional[GcpSqlDBInstanceSettingsIPConfig]
+    location_preference: Optional[GcpSqlDBInstanceSettingsLocPref]
+    maintenance_window: Optional[GcpSqlDBInstanceSettingsMaintWindow]
+    insights_config: Optional[GcpSqlDBInstanceSettingsInsights]
+
+
+@dataclass
+class GcpSqlDBInstanceReplicaConfig:
+    """
+        Attributes:
+            ca_certificate: (Optional) PEM representation of the trusted CA's x509 certificate.
+            client_certificate: (Optional) PEM representation of the replica's x509 certificate.
+            client_key: (Optional) PEM representation of the replica's private key. The corresponding public key in encoded in the client_certificate.
+            connect_retry_interval: (Optional, Default: 60) The number of seconds between connect retries.
+            dump_file_path: (Optional) Path to a SQL file in GCS from which replica instances are created. Format is gs://bucket/filename.
+            failover_target: (Optional) Specifies if the replica is the failover target.
+            master_heartbeat_period: (Optional) Time in ms between replication heartbeats.
+            password: (Optional) Password for the replication connection.
+            sslCipher: (Optional) Permissible ciphers for use in SSL encryption.
+            username: (Optional) Username for replication connection.
+            verify_server_certificate: (Optional) True if the master's common name value is checked during the SSL handshake.
+    """
+    ca_certificate: Optional[str]
+    client_certificate: Optional[str]
+    client_key: Optional[str]
+    connect_retry_interval: Optional[int]
+    dump_file_path: Optional[str]
+    failover_target: Optional[bool]
+    master_heartbeat_period: Optional[int]
+    password: Optional[str]
+    sslCipher: Optional[str]
+    username: Optional[str]
+    verify_server_certificate: Optional[bool]
+
+
+@dataclass
+class GcpSqlDBInstanceRestoreBackupContext:
+    """
+        Attributes:
+            backup_run_id: (Required) The ID of the backup run to restore from.
+            instance_id: (Optional) The ID of the instance that the backup was taken from. If left empty, this instance's ID will be used.
+            project: (Optional) The full project ID of the source instance.
+    """
+    backup_run_id: str
+    instance_id: Optional[str]
+    project: Optional[str]
+
+
+@dataclass
+class GcpSqlDBInstanceClone:
+    """
+        Attributes:
+            source_instance_name: (Required) Name of the source instance which will be cloned.
+            point_in_time: (Optional) The timestamp of the point in time that should be restored.
+                                      A timestamp in RFC3339 UTC "Zulu" format, with nanosecond resolution and up to nine fractional digits.
+                                      Examples: "2014-10-02T15:01:23Z" and "2014-10-02T15:01:23.045123456Z".
+    """
+    source_instance_name: str
+    point_in_time: Optional[datetime]
+
+
 class GcpSqlDatabaseInstance(GcpResource):
     """
         Attributes:
-            name: The name of this SQL database instance
-            require_ssl: An indication on if this instance requires SSL
+            name: (Optional, Computed) The name of this SQL database instance.
+            region: (Optional) The region where this instance resides.
+            settings: (Optional) The settings used for the sql instance.
+            database_version: (Optional, Default: MYSQL_5_6) The version of the sql database.
+            master_instance_name: (Optional) The name of the instance that will act as master in replication setup.
+            project: (Optional) The ID of the project.
+            replica_configuration: (Optional) The configuration for replication. Applicable to MySql instances only.
+            root_password: (Optional) The initial root password, applicable to MS Sql instances only.
+            encryption_key_name: (Optional) The full path to the encryption key used for the CMEK disk encryption.
+            deletion_protection: (Optional, Default: true) The optional setting to whether or not allow Terraform to destroy the instance.
+            restore_backup_context: (Optional) The context needed to restore the database to a backup run.
+            clone: (Optional) The context needed to create this instance as a clone of another instance.
     """
 
-    def __init__(self, name: str, require_ssl: bool):
+    def __init__(self,
+                 name: str,
+                 region: str,
+                 settings: Optional[GcpSqlDBInstanceSettings],
+                 database_version: Optional[GcpSqlDBInstanceVersion],
+                 master_instance_name: str,
+                 project: str,
+                 replica_configuration: Optional[GcpSqlDBInstanceReplicaConfig],
+                 root_password: str,
+                 encryption_key_name: str,
+                 deletion_protection: bool,
+                 restore_backup_context: Optional[GcpSqlDBInstanceRestoreBackupContext],
+                 clone: Optional[GcpSqlDBInstanceClone]):
+
         super().__init__(GcpResourceType.GOOGLE_SQL_DATABASE_INSTANCE)
-        self.name: str = name
-        self.require_ssl: bool = require_ssl
+        self.name: Optional[str] = name
+        self.region: Optional[str] = region
+        self.settings: Optional[GcpSqlDBInstanceSettings] = settings
+        self.database_version: Optional[GcpSqlDBInstanceVersion] = database_version
+        self.replica_configuration: Optional[GcpSqlDBInstanceReplicaConfig] = replica_configuration
+        self.root_password: Optional[str] = root_password
+        self.deletion_protection: Optional[bool] = deletion_protection
+        self.restore_backup_context: Optional[GcpSqlDBInstanceRestoreBackupContext] = restore_backup_context
+        self.clone: Optional[GcpSqlDBInstanceClone] = clone
+
+        # References to other resources
+        self.master_instance_name: Optional[str] = master_instance_name
+        self.project: str = Optional[project]
+        self.encryption_key_name: Optional[str] = encryption_key_name
 
     def get_keys(self) -> List[str]:
         return [self.name, self.project_id]
--- cloudrail/knowledge/context/gcp/resources_builders/terraform/sql_database_instance_builder.py
+++ cloudrail/knowledge/context/gcp/resources_builders/terraform/sql_database_instance_builder.py
@@ -1,15 +1,169 @@
-from cloudrail.knowledge.context.gcp.resources.constants.gcp_resource_type import GcpResourceType
-from cloudrail.knowledge.context.gcp.resources.sql.gcp_sql_database_instance import GcpSqlDatabaseInstance
-
 from cloudrail.knowledge.context.gcp.resources_builders.terraform.base_gcp_terraform_builder import BaseGcpTerraformBuilder
+from cloudrail.knowledge.context.gcp.resources.sql.gcp_sql_database_instance import *
+from cloudrail.knowledge.context.gcp.resources.sql.gcp_sql_database_instance import GcpSqlDBInstanceSettingsDBFlags
+
+from datetime import datetime
 
 
 class SqlDatabaseInstanceBuilder(BaseGcpTerraformBuilder):
 
     def do_build(self, attributes: dict) -> GcpSqlDatabaseInstance:
-        require_ssl = attributes.get('settings', [{}])[0].get('ip_configuration', [{}])[0].get('require_ssl', False)
-        return GcpSqlDatabaseInstance(attributes['name'],
-                                      require_ssl)
+        settings = self.build_settings_block(attributes)
+        database_version = GcpSqlDBInstanceVersion(attributes.get("database_version", "MYSQL_5_6"))
+        replica_configuration = self.build_replica_configuration(attributes)
+        restore_backup_context = self.build_restore_backup_context(attributes)
+        clone = self.build_clone(attributes)
+
+        return GcpSqlDatabaseInstance(name=attributes.get("name", None),
+                                      region=attributes.get("region", None),
+                                      settings=settings,
+                                      database_version=database_version,
+                                      replica_configuration=replica_configuration,
+                                      root_password=attributes.get("root_password", None),
+                                      deletion_protection=attributes.get("deletion_protection", True),
+                                      restore_backup_context=restore_backup_context,
+                                      clone=clone,
+                                      master_instance_name=attributes.get("master_instance_name", None),
+                                      project=attributes.get("project", None),
+                                      encryption_key_name=attributes.get("encryption_key_name", None))
 
     def get_service_name(self) -> GcpResourceType:
         return GcpResourceType.GOOGLE_SQL_DATABASE_INSTANCE
+
+    def build_settings_block(self, attributes: dict):
+        settings_block = self._get_known_value(attributes, "settings")
+        settings = settings_block[0] if settings_block else None
+
+        if settings:
+            tier = settings.get("tier", None)
+            activation_policy = self._get_known_value(settings, "activation_policy")
+            authorized_gae_applications = self._get_known_value(settings, "authorized_gae_applications")
+            availability_type = self._get_known_value(settings, "availability_type")
+            collation = self._get_known_value(settings, "collation")
+            crash_safe_replication = self._get_known_value(settings, "crash_safe_replication")
+            disk_autoresize = self._get_known_value(settings, "disk_autoresize", True)
+            disk_size = self._get_known_value(settings, "disk_size", 10)
+            disk_type = self._get_known_value(settings, "disk_type", "PD_SSD")
+            pricing_plan = self._get_known_value(settings, "pricing_plan")
+            replication_type = self._get_known_value(settings, "replication_type")
+            user_labels = self._get_known_value(settings, "user_labels")
+            database_flags_list = self._get_known_value(settings, "database_flags")
+            database_flags = [GcpSqlDBInstanceSettingsDBFlags(database_flag["name"], database_flag["value"])
+                              for database_flag in database_flags_list] if database_flags_list else None
+            backup_configuration = self.build_backup_configuration(settings)
+            ip_configuration = self.build_ip_configuration(settings)
+            location_preference_block = self._get_known_value(settings, "location_preference")
+            location_preference = location_preference_block[0] if location_preference_block else None
+            location_preference = GcpSqlDBInstanceSettingsLocPref(location_preference.get("follow_gae_application", None),
+                                                                  location_preference.get("zone", None)) if location_preference else None
+            maintenance_window_block = self._get_known_value(settings, "maintenance_window")
+            maintenance_window = maintenance_window_block[0] if maintenance_window_block else None
+            maintenance_window = GcpSqlDBInstanceSettingsMaintWindow(maintenance_window.get("day", None),
+                                                                     maintenance_window.get("hour", None),
+                                                                     maintenance_window.get("update_track", None)) if maintenance_window else None
+            insights_config_block = self._get_known_value(settings, "insights_config")
+            insights_config = insights_config_block[0] if insights_config_block else None
+            insights_config = GcpSqlDBInstanceSettingsInsights(insights_config.get("query_insights_enabled", False),
+                                                               insights_config.get("query_string_length", 1024),
+                                                               insights_config.get("record_application_tags", False),
+                                                               insights_config.get("record_client_address", False)) if insights_config else None
+
+            return GcpSqlDBInstanceSettings(tier, activation_policy, authorized_gae_applications, crash_safe_replication,
+                                            availability_type, collation, disk_autoresize,
+                                            disk_size, disk_type, pricing_plan, replication_type,
+                                            user_labels, database_flags, backup_configuration, ip_configuration, location_preference,
+                                            maintenance_window, insights_config)
+
+        return None
+
+    def build_backup_configuration(self, settings: dict):
+        backup_configuration_block = self._get_known_value(settings, "backup_configuration")
+        backup_configuration = backup_configuration_block[0] if backup_configuration_block else None
+
+        if backup_configuration:
+            binary_log_enabled = self._get_known_value(backup_configuration, "binary_log_enabled")
+            enabled = self._get_known_value(backup_configuration, "enabled")
+            start_time_str = self._get_known_value(backup_configuration, "start_time")
+            start_time = datetime.strptime(start_time_str, "%H:%M") if start_time_str else None
+            point_in_time_recovery_enabled = self._get_known_value(backup_configuration, "point_in_time_recovery_enabled")
+            location = self._get_known_value(backup_configuration, "location")
+            transaction_log_retention_days = self._get_known_value(backup_configuration, "transaction_log_retention_days")
+            backup_retention_settings_block = self._get_known_value(backup_configuration, "backup_retention_settings")
+            backup_retention_settings = GcpSqlDBInstanceSettingsBackupRetention(backup_retention_settings_block[0]["retained_backups"],
+                                                                                backup_retention_settings_block[0].get("retention_unit", "COUNT")) if backup_retention_settings_block else None
+
+            return GcpSqlDBInstanceSettingsBackupConfig(binary_log_enabled, enabled, start_time, point_in_time_recovery_enabled,
+                                                        location, transaction_log_retention_days, backup_retention_settings)
+        return None
+
+    def build_ip_configuration(self, settings: dict):
+        ip_configuration_block = self._get_known_value(settings, "ip_configuration")
+        ip_configuration = ip_configuration_block[0] if ip_configuration_block else None
+
+        if ip_configuration:
+            ipv4_enabled = self._get_known_value(ip_configuration, "ipv4_enabled")
+            private_network = ip_configuration.get("private_network", None)
+            require_ssl = self._get_known_value(ip_configuration, "require_ssl")
+            authorized_networks_list = self._get_known_value(ip_configuration, "authorized_networks")
+            authorized_networks = [self.build_authorized_network(authorized_network)
+                                   for authorized_network in authorized_networks_list] if authorized_networks_list else None
+
+            return GcpSqlDBInstanceSettingsIPConfig(ipv4_enabled, private_network, require_ssl, authorized_networks)
+
+        return None
+
+    def build_authorized_network(self, authorized_network: dict) -> GcpSqlDBInstanceIPConfigAuthNetworks:
+        expiration_time_str = self._get_known_value(authorized_network, "expiration_time")
+        expiration_time = datetime.strptime(expiration_time_str.split(".")[0], '%Y-%m-%dT%H:%M:%S') if expiration_time_str else None
+
+        return GcpSqlDBInstanceIPConfigAuthNetworks(expiration_time,
+                                                    self._get_known_value(authorized_network, "name"),
+                                                    authorized_network["value"])
+
+    def build_replica_configuration(self, attributes: dict):
+        replica_configuration_block = self._get_known_value(attributes, "replica_configuration")
+        replica_configuration = replica_configuration_block[0] if replica_configuration_block else None
+
+        if replica_configuration:
+            ca_certificate = self._get_known_value(replica_configuration, "ca_certificate")
+            client_certificate = self._get_known_value(replica_configuration, "client_certificate")
+            client_key = self._get_known_value(replica_configuration, "client_key")
+            connect_retry_interval = self._get_known_value(replica_configuration, "connect_retry_interval", 60)
+            dump_file_path = self._get_known_value(replica_configuration, "dump_file_path")
+            failover_target = self._get_known_value(replica_configuration, "failover_target")
+            master_heartbeat_period = self._get_known_value(replica_configuration, "master_heartbeat_period")
+            password = self._get_known_value(replica_configuration, "password")
+            sslCipher = self._get_known_value(replica_configuration, "sslCipher")
+            username = self._get_known_value(replica_configuration, "username")
+            verify_server_certificate = self._get_known_value(replica_configuration, "verify_server_certificate")
+
+            return GcpSqlDBInstanceReplicaConfig(ca_certificate, client_certificate, client_key, connect_retry_interval, dump_file_path,
+                                                 failover_target, master_heartbeat_period, password, sslCipher, username, verify_server_certificate)
+
+        return None
+
+    def build_restore_backup_context(self, attributes: dict):
+        restore_backup_context_block = self._get_known_value(attributes, "restore_backup_context")
+        restore_backup_context = restore_backup_context_block[0] if restore_backup_context_block else None
+
+        if restore_backup_context:
+            backup_run_id = restore_backup_context["backup_run_id"]
+            instance_id = restore_backup_context.get("instance_id", None)
+            project = restore_backup_context.get("project", None)
+
+            return GcpSqlDBInstanceRestoreBackupContext(backup_run_id, instance_id, project)
+
+        return None
+
+    def build_clone(self, attributes: dict):
+        clone_block = self._get_known_value(attributes, "clone")
+        clone = clone_block[0] if clone_block else None
+
+        if clone:
+            source_instance_name = clone["source_instance_name"]
+            point_in_time_str = self._get_known_value(clone, "point_in_time")
+            point_in_time = datetime.strptime(point_in_time_str.split(".")[0], '%Y-%m-%dT%H:%M:%S') if point_in_time_str else None
+
+            return GcpSqlDBInstanceClone(source_instance_name, point_in_time)
+
+        return None
