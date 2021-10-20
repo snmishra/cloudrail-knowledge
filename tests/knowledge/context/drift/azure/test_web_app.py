45e9c8c0e08cbbcaf382e28008be6dbb12c13ed9
Imanuel <imanuel.m@indeni.com>
2021-10-20 09:36:38 +0300

    CR-3248: modified test scenarios following PR comments

--- tests/knowledge/context/drift/azure/test_web_app.py
+++ tests/knowledge/context/drift/azure/test_web_app.py
@@ -12,20 +12,26 @@ class TestWebApp(BaseAzureDriftTest):
     @drift_test(module_path="ftps_state")
     def test_ftps_state(self, results: List[Drift]):
         self.assertEqual(len(results), 1)
-        web_app = next(res for res in results if res.resource_id == 'azurerm_app_service.webapp')
+        web_app = next((res for res in results if res.resource_id == 'azurerm_app_service.webapp'), None)
+        self.assertIsNotNone(web_app.resource_iac['app_service_config'])
+        self.assertIsNotNone(web_app.resource_live['app_service_config'])
         self.assertEqual(web_app.resource_iac['app_service_config']['ftps_state'], 'FtpsOnly')
         self.assertEqual(web_app.resource_live['app_service_config']['ftps_state'], 'AllAllowed')
 
     @drift_test(module_path="authentication_check")
     def test_authentication_check(self, results: List[Drift]):
         self.assertEqual(len(results), 1)
-        web_app = next(res for res in results if res.resource_id == 'azurerm_app_service.webapp')
+        web_app = next((res for res in results if res.resource_id == 'azurerm_app_service.webapp'), None)
+        self.assertIsNotNone(web_app.resource_iac['app_service_config']['auth_settings']['enabled'])
+        self.assertIsNotNone(web_app.resource_live['app_service_config']['auth_settings']['enabled'])
         self.assertTrue(web_app.resource_iac['app_service_config']['auth_settings']['enabled'])
         self.assertFalse(web_app.resource_live['app_service_config']['auth_settings']['enabled'])
 
     @drift_test(module_path="latest_tls")
     def test_latest_tls(self, results: List[Drift]):
         self.assertEqual(len(results), 1)
-        web_app = next(res for res in results if res.resource_id == 'azurerm_app_service.webapp')
+        web_app = next((res for res in results if res.resource_id == 'azurerm_app_service.webapp'), None)
+        self.assertIsNotNone(web_app.resource_iac['app_service_config'])
+        self.assertIsNotNone(web_app.resource_live['app_service_config'])
         self.assertEqual(web_app.resource_iac['app_service_config']['minimum_tls_version'], '1.2')
         self.assertEqual(web_app.resource_live['app_service_config']['minimum_tls_version'], '1.1')
