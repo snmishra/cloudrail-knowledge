8fedb75cb11ba8f595cd5644d617299013a077aa
Imanuel <imanuel.m@indeni.com>
2021-10-20 19:23:54 +0300

    CR-3379: added drift check where we assign task definition

--- cloudrail/knowledge/context/aws/resources/ecs/ecs_service.py
+++ cloudrail/knowledge/context/aws/resources/ecs/ecs_service.py
@@ -91,4 +91,5 @@ class EcsService(NetworkEntity, INetworkConfiguration, IEcsInstance):
                 'launch_type': self.launch_type.value,
                 'network_conf_list': [conf.to_dict() for conf in self.network_conf_list],
                 'task_definition_arn': self.task_definition_arn,
-                'cluster_name': self.cluster_name}
+                'cluster_name': self.cluster_name,
+                'task_definition': self._task_definition.to_drift_detection_object()}
--- cloudrail/knowledge/context/aws/resources/ecs/ecs_target.py
+++ cloudrail/knowledge/context/aws/resources/ecs/ecs_target.py
@@ -83,4 +83,5 @@ class EcsTarget(NetworkEntity, INetworkConfiguration, IEcsInstance):
                 'cluster_arn': self.cluster_arn,
                 'role_arn': self.role_arn,
                 'network_conf_list': [conf.to_dict() for conf in self.network_conf_list],
-                'cluster_name': self.cluster_name}
+                'cluster_name': self.cluster_name,
+                'task_definition': self._task_definition.to_drift_detection_object()}
