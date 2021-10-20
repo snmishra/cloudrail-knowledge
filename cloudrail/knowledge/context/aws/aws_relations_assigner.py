25baa984974122b1de9f96090e87ce2aa2b02541
Imanuel <imanuel.m@indeni.com>
2021-10-20 14:39:18 +0300

    CR-3379: added some files for review

--- cloudrail/knowledge/context/aws/aws_relations_assigner.py
+++ cloudrail/knowledge/context/aws/aws_relations_assigner.py
@@ -2206,9 +2206,10 @@ class AwsRelationsAssigner(DependencyInvocation):
     ## To avoid drifts, assigning tags for such INACTIVE task definitions.
     @staticmethod
     def _ecs_task_arn_check(resource: EcsTaskDefinition, tags_arn: str) -> bool:
-        return 'task-definition/' in tags_arn and resource.status == TaskDefinitionStatus.INACTIVE \
-                and resource.task_arn.split(':')[-1].isnumeric() and tags_arn.split(':')[-1].isnumeric() \
-                    and (':').join(resource.task_arn.split(':')[:-1]) == (':').join(tags_arn.split(':')[:-1])
+        return False
+        # return 'task-definition/' in tags_arn and resource.status == TaskDefinitionStatus.INACTIVE \
+        #         and resource.task_arn.split(':')[-1].isnumeric() and tags_arn.split(':')[-1].isnumeric() \
+        #             and (':').join(resource.task_arn.split(':')[:-1]) == (':').join(tags_arn.split(':')[:-1])
 
     @staticmethod
     def _assign_s3_bucket_objects(bucket_object: S3BucketObject, buckets: AliasesDict[S3Bucket]):
