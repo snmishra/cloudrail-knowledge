0c670a9d0334dcc29f47a787c7e4c0fa39527bae
Imanuel <imanuel.m@indeni.com>
2021-10-25 12:17:31 +0300

    CR-3262: renamed the tests, not the path

--- tests/knowledge/rules/gcp/non_context_aware/compute_instance_launch_with_vm_shield_rule/test_compute_instance_launch_with_vm_shield_rule.py
+++ tests/knowledge/rules/gcp/non_context_aware/compute_instance_launch_with_vm_shield_rule/test_compute_instance_launch_with_vm_shield_rule.py
@@ -7,18 +7,18 @@ class TestComputeInstanceLaunchWithVmShieldRule(GcpBaseRuleTest):
     def get_rule(self):
         return ComputeInstanceLaunchWithVmShieldRule()
 
-    @rule_test('both_vptm_and_integrity_disabled_secure_boot_enabled', should_alert=True)
-    def test_enable_neither_vptm_nor_integrity(self, rule_result: RuleResponse):
+    @rule_test('enable_neither_vptm_nor_integrity', should_alert=True)
+    def test_both_vptm_and_integrity_disabled_secure_boot_enabled(self, rule_result: RuleResponse):
         pass
 
-    @rule_test('all_attributes_enabled', should_alert=False)
-    def test_enable_vtpm_and_integrity(self, rule_result: RuleResponse):
+    @rule_test('enable_vtpm_and_integrity', should_alert=False)
+    def test_all_attributes_enabled(self, rule_result: RuleResponse):
         pass
 
-    @rule_test('vtpm_and_secure_boot_enabled_integrity_disabled', should_alert=True)
-    def test_enable_vtpm_not_integrity(self, rule_result: RuleResponse):
+    @rule_test('enable_vtpm_not_integrity', should_alert=True)
+    def test_vtpm_and_secure_boot_enabled_integrity_disabled(self, rule_result: RuleResponse):
         pass
 
-    @rule_test('vtpm_and_integrity_enabled_secure_boot_disabled', should_alert=True)
-    def test_no_secure_boot(self, rule_result: RuleResponse):
+    @rule_test('no_secure_boot', should_alert=True)
+    def test_vtpm_and_integrity_enabled_secure_boot_disabled(self, rule_result: RuleResponse):
         pass
