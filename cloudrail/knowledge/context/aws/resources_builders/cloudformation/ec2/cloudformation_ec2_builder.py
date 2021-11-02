475b2c552c50020bbbe8fbeaa910d28e159dbe6b
Imanuel <imanuel.m@indeni.com>
2021-11-02 12:20:09 +0200

    CR-3430: modified ec2 builder

--- cloudrail/knowledge/context/aws/resources_builders/cloudformation/ec2/cloudformation_ec2_builder.py
+++ cloudrail/knowledge/context/aws/resources_builders/cloudformation/ec2/cloudformation_ec2_builder.py
@@ -21,15 +21,18 @@ class CloudformationEc2Builder(BaseCloudformationBuilder):
         network_interface_ids = network_interface_ids if check_array_has_value(network_interface_ids) else self.CFN_PSEUDO_LIST
         security_group_ids_from_enis = flat_list([ni.get('GroupSet') for ni in network_interfaces])
         security_group_ids_from_enis = set(sg for sg in security_group_ids_from_enis)
-        security_groups_ids = self.get_property(properties, 'SecurityGroupIds') \
-            or security_group_ids_from_enis if check_array_has_value(security_group_ids_from_enis) else self.CFN_PSEUDO_LIST
+        security_groups_ids_from_resource = self.get_property(properties, 'SecurityGroupIds')
+        security_groups_ids_from_resource = security_groups_ids_from_resource \
+            if check_array_has_value(security_groups_ids_from_resource) and all('.' not in x for x in security_groups_ids_from_resource) else None
+        security_groups_ids = security_groups_ids_from_resource \
+            or (security_group_ids_from_enis if check_array_has_value(security_group_ids_from_enis) else None)
         associate_public_ip_address_data = [ni.get('AssociatePublicIpAddress') for ni in network_interfaces if ni.get('DeviceIndex') == '0']
         associate_public_ip_address = AssociatePublicIpAddress.convert_from_optional_boolean(associate_public_ip_address_data[0]
                                                                                              if check_array_has_value(associate_public_ip_address_data)
                                                                                              else None)
         subnet_id_from_resource = self.get_property(properties, 'SubnetId')
         subnet_ids_from_enis = [ni.get('SubnetId') for ni in network_interfaces]
-        subnet_id = subnet_id_from_resource or subnet_ids_from_enis[0] if check_array_has_value(subnet_ids_from_enis) else None
+        subnet_id = subnet_id_from_resource or (subnet_ids_from_enis[0] if check_array_has_value(subnet_ids_from_enis) else None)
         http_tokens = 'optional'
         ebs_optimized = self.get_property(properties, 'EbsOptimized', False)
 
