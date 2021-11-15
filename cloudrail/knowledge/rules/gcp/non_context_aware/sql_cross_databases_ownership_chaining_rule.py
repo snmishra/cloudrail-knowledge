898b06b1086503fda58a05ae7d1f3c882edc908a
Idan Aviv <idana@indeni.com>
2021-11-15 17:44:00 +0200

    syntax fixes

--- cloudrail/knowledge/rules/gcp/non_context_aware/sql_cross_databases_ownership_chaining_rule.py
+++ cloudrail/knowledge/rules/gcp/non_context_aware/sql_cross_databases_ownership_chaining_rule.py
@@ -18,7 +18,7 @@ class SqlCrossDatabasesOwnershipChainingRule(BaseDatabaseFlagOnRule):
                     self.is_flag_on(sql_db, 'cross db ownership chaining'):
                 issues.append(
                     Issue(
-                        f"The Google Cloud database instance `{sql_db.get_friendly_name()}` has database"
+                        f"The Google Cloud {sql_db.get_type()} `{sql_db.get_friendly_name()}` has database"
                         f" flag `cross db ownership chaining` set to on.",
                         sql_db,
                         sql_db))
