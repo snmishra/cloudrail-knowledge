58109a382cf3df0c10dda1759521cf367783ea8c
Idan Aviv <idana@indeni.com>
2021-11-16 17:07:30 +0200

    TestPostgresLogMinDuration: fix

--- tests/knowledge/rules/gcp/non_context_aware/postgres_log_min_duration/test_postgres_log_min_duration.py
+++ tests/knowledge/rules/gcp/non_context_aware/postgres_log_min_duration/test_postgres_log_min_duration.py
@@ -8,10 +8,10 @@ class TestPostgresLogMinDuration(GcpBaseRuleTest):
     def get_rule(self):
         return SqlLogMinimumDurationDisableRule()
 
-    @rule_test('log_min_duration_off', should_alert=True)
+    @rule_test('log_min_duration_off', should_alert=False)
     def test_log_min_duration_off(self, rule_result: RuleResponse):
         pass
 
-    @rule_test('log_min_duration_on', should_alert=False)
+    @rule_test('log_min_duration_on', should_alert=True)
     def test_log_min_duration_on(self, rule_result: RuleResponse):
         pass
