b1133eb99682926fefe7deb52448df22baae0a36
Doron Afota <doronafota@Dorons-MacBook-Pro.local>
2021-12-02 08:46:22 +0200

    gcp car rule Ensure no HTTPS or SSL proxy load balancers permit SSL policies with weak cipher suites

--- cloudrail/knowledge/rules/gcp/context_aware/compute_ssl_policy_proxy_no_weak_ciphers_rule.py
+++ cloudrail/knowledge/rules/gcp/context_aware/compute_ssl_policy_proxy_no_weak_ciphers_rule.py
@@ -15,25 +15,25 @@ class ComputeSslPolicyProxyNoWeakCiphersRule(GcpBaseRule):
         not_secure_ciphers = ["TLS_RSA_WITH_AES_128_GCM_SHA256", "TLS_RSA_WITH_AES_256_GCM_SHA384", "TLS_RSA_WITH_AES_128_CBC_SHA", "TLS_RSA_WITH_AES_256_CBC_SHA", "TLS_RSA_WITH_3DES_EDE_CBC_SHA"]
         for global_forwarding_rule in env_context.compute_global_forwarding_rule:
             if global_forwarding_rule.target.is_encrypted:
-                target_proxy = global_forwarding_rule.target
-                if target_proxy.ssl_policy:
-                    if target_proxy.ssl_policy.min_tls_version == "TLS_1_2":
-                        if target_proxy.ssl_policy.profile == "MODERN" or target_proxy.ssl_policy.profile == "RESTRICTED" or \
-                            (target_proxy.ssl_policy.profile == "CUSTOM" and
-                             all(custom_feature not in not_secure_ciphers for custom_feature in target_proxy.ssl_policy.custom_features)):
+                if global_forwarding_rule.target.ssl_policy:
+                    if global_forwarding_rule.target.ssl_policy.min_tls_version == "TLS_1_2":
+                        if global_forwarding_rule.target.ssl_policy.profile == "MODERN" or global_forwarding_rule.target.ssl_policy.profile == "RESTRICTED" or \
+                            (global_forwarding_rule.target.ssl_policy.profile == "CUSTOM" and
+                             all(custom_feature not in not_secure_ciphers for custom_feature in global_forwarding_rule.target.ssl_policy.custom_features)):
                             evidence_string = ""
                         else:
                             evidence_string = f"The {global_forwarding_rule.get_type()} '{global_forwarding_rule.get_friendly_name()}' is using TLS version less that 1.2 in target " \
-                                              f"{target_proxy.target_type} proxy {target_proxy.get_friendly_name()} with a misconfigured SSL policy {target_proxy.ssl_policy.get_friendly_name()}"
+                                              f"{global_forwarding_rule.target.target_type} proxy {target_proxy.get_friendly_name()} " \
+                                              f"with a misconfigured SSL policy {global_forwarding_rule.target.ssl_policy.get_friendly_name()}"
                     else:
-                        evidence_string = f"The {global_forwarding_rule.get_type()} is using weak ciphers in target {target_proxy.target_type} " \
-                                          f"proxy {target_proxy.get_friendly_name()} with a misconfigured SSL policy {target_proxy.ssl_policy.get_friendly_name()}"
+                        evidence_string = f"The {global_forwarding_rule.get_type()} is using weak ciphers in target {global_forwarding_rule.target.target_type} " \
+                                          f"proxy {global_forwarding_rule.target.get_friendly_name()} with a misconfigured SSL policy {global_forwarding_rule.target.ssl_policy.get_friendly_name()}"
                     if evidence_string:
                         issues.append(
                             Issue(
                                 evidence_string,
                                 global_forwarding_rule,
-                                target_proxy.ssl_policy))
+                                global_forwarding_rule.target.ssl_policy))
                 else:
                     issues.append(
                         Issue(
