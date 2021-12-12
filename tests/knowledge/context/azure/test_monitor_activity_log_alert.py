c655fa8d24ca51448e092df58cf780601c2bcd30
Doron Afota <doronafota@Dorons-MacBook-Pro.local>
2021-12-12 09:20:37 +0200

    azure monitor activity log alert

--- tests/knowledge/context/azure/test_monitor_activity_log_alert.py
+++ tests/knowledge/context/azure/test_monitor_activity_log_alert.py
@@ -30,19 +30,19 @@ class TestMonitorActivityLogAlert(AzureContextTest):
         self.assertEqual(monitor.criteria.recommendation_category.value, 'OperationalExcellence')
         self.assertEqual(monitor.criteria.recommendation_impact.value, 'High')
         self.assertIsNone(monitor.criteria.recommendation_type)
-        for action in monitor.actions or []:
+        for action in monitor.actions:
             self.assertEqual(action.webhook_properties, {'key': 'value'})
         self.assertEqual(monitor.tags, {'environment': 'production'})
         self.assertIsNone(monitor.criteria.service_health)
 
         if monitor.origin == EntityOrigin.TERRAFORM:
             self.assertEqual(monitor.criteria.resource_id, 'azurerm_network_security_group.nsg.id')
-            for action in monitor.actions or []:
+            for action in monitor.actions:
                 self.assertTrue(action.action_group_id in ['azurerm_monitor_action_group.test.id', 'azurerm_monitor_action_group.test10.id'])
         elif monitor.origin == EntityOrigin.LIVE_ENV:
             self.assertEqual(monitor.criteria.resource_id, '/subscriptions/230613d8-3b34-4790-b650-36f31045f19a/resourceGroups/cr3690-RG/providers/Microsoft.'
                                                            'Network/networkSecurityGroups/cr3690-nsg')
-            for action in monitor.actions or []:
+            for action in monitor.actions:
                 self.assertTrue(action.action_group_id in ['/subscriptions/230613d8-3b34-4790-b650-36f31045f19a/resourceGroups/cr3690-RG/providers/Microsoft.'
                                                                    'Insights/actionGroups/doron_test',
                                                                    '/subscriptions/230613d8-3b34-4790-b650-36f31045f19a/resourceGroups/cr3690-RG/providers/Microsoft.'
