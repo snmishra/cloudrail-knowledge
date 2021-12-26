b9c21087100c07187657e86d8d685092db55109c
Imanuelm <imanuel.m@indeni.com>
2021-12-26 11:30:01 +0200

    CR-3672: modified python test rule

--- tests/knowledge/rules/azure/context_aware/test_vmss_diagnostics_logs_enabled_rule.py
+++ tests/knowledge/rules/azure/context_aware/test_vmss_diagnostics_logs_enabled_rule.py
@@ -17,20 +17,26 @@ class TestVmssDiagnosticsLogsEnabledRule(TestCase):
 
     @parameterized.expand(
         [
-            ["no_logs_enabled", False, True],
-            ["logs_enabled", True, False],
+            ["no_logs_enabled_windows", OperatingSystemType.WINDOWS,
+             'Microsoft.Azure.Applications', 'IaaS47C6E03DTest', True],
+            ["logs_enabled_windows", OperatingSystemType.WINDOWS,
+             'Microsoft.Azure.Diagnostics', 'IaaSDiagnostics', False],
+            ["no_logs_enabled_linux", OperatingSystemType.LINUX,
+             'Microsoft.HpcPack', 'LinuxNodeAgent', True],
+            ["logs_enabled_linux", OperatingSystemType.LINUX,
+             'Microsoft.OSTCExtensions', 'LinuxDiagnostic', False]
         ]
     )
-    def test_vmss_diagnostics_logs(self, unused_name: str, extnesion_configured: bool, should_alert: bool):
+    def test_vmss_diagnostics_logs(self, unused_name: str, os_type: OperatingSystemType, publisher: str,
+                                   extension_type: str, should_alert: bool):
         # Arrange
         virtual_machines_scales_set: AzureVirtualMachineScaleSet = create_empty_entity(AzureVirtualMachineScaleSet)
         virtual_machines_scales_set.name = 'vmss-test'
-        virtual_machines_scales_set.os_type = OperatingSystemType.WINDOWS
+        virtual_machines_scales_set.os_type = os_type
         vmss_extension: AzureVirtualMachineExtension = create_empty_entity(AzureVirtualMachineExtension)
-        if extnesion_configured:
-            vmss_extension.publisher = 'Microsoft.Azure.Diagnostics'
-            vmss_extension.extension_type = 'IaaSDiagnostics'
-            virtual_machines_scales_set.extensions.append(vmss_extension)
+        vmss_extension.publisher = publisher
+        vmss_extension.extension_type = extension_type
+        virtual_machines_scales_set.extensions.append(vmss_extension)
         context = AzureEnvironmentContext(virtual_machines_scale_sets=AliasesDict(virtual_machines_scales_set),
                                           vms_extentions=AliasesDict(vmss_extension))
         # Act
