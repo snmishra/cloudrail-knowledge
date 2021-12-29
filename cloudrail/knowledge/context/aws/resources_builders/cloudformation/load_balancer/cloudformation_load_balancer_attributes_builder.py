5dfb3572ecba5f97982758314bc283d6e6eb8afe
Imanuelm <imanuel.m@indeni.com>
2021-12-29 18:45:00 +0200

    CR-3214: fixed builder to return only if needed

--- cloudrail/knowledge/context/aws/resources_builders/cloudformation/load_balancer/cloudformation_load_balancer_attributes_builder.py
+++ cloudrail/knowledge/context/aws/resources_builders/cloudformation/load_balancer/cloudformation_load_balancer_attributes_builder.py
@@ -10,21 +10,23 @@ class CloudformationLoadBalancerAttributesBuilder(BaseCloudformationBuilder):
         super().__init__(CloudformationResourceType.ELASTIC_LOAD_BALANCER, cfn_by_type_map)
 
     def parse_resource(self, cfn_res_attr: dict) -> LoadBalancerAttributes:
-        properties: dict = cfn_res_attr['Properties']
-        account = cfn_res_attr['account_id']
-        region = cfn_res_attr['region']
-        load_balancer_arn = self.get_resource_id(cfn_res_attr)
-        attributes_dict = {}
-        for attribute in self.get_property(properties, 'LoadBalancerAttributes', []):
-            if attribute['Value'] == 'true':
-                attributes_dict.update({attribute['Key']: True})
-            elif attribute['Value'] == 'false':
-                attributes_dict.update({attribute['Key']: False})
-            else:
-                attributes_dict.update({attribute['Key']: attribute['Value']})
-        drop_invalid_header_fields = attributes_dict.get('routing.http.drop_invalid_header_fields.enabled', False)
-        access_logs = LoadBalancerAccessLogs(attributes_dict.get('access_logs.s3.bucket', ''),
-                                             attributes_dict.get('access_logs.s3.prefix', ''),
-                                             attributes_dict.get('access_logs.s3.enabled', False))
-        return LoadBalancerAttributes(account=account, region=region, load_balancer_arn=load_balancer_arn,
+        if self.get_property(cfn_res_attr['Properties'], 'LoadBalancerAttributes'):
+            properties: dict = cfn_res_attr['Properties']
+            account = cfn_res_attr['account_id']
+            region = cfn_res_attr['region']
+            load_balancer_arn = self.get_resource_id(cfn_res_attr)
+            attributes_dict = {}
+            for attribute in self.get_property(properties, 'LoadBalancerAttributes', []):
+                if attribute['Value'] == 'true':
+                    attributes_dict.update({attribute['Key']: True})
+                elif attribute['Value'] == 'false':
+                    attributes_dict.update({attribute['Key']: False})
+                else:
+                    attributes_dict.update({attribute['Key']: attribute['Value']})
+            drop_invalid_header_fields = attributes_dict.get('routing.http.drop_invalid_header_fields.enabled', False)
+            access_logs = LoadBalancerAccessLogs(attributes_dict.get('access_logs.s3.bucket', ''),
+                                                 attributes_dict.get('access_logs.s3.prefix', ''),
+                                                 attributes_dict.get('access_logs.s3.enabled', False))
+            return LoadBalancerAttributes(account=account, region=region, load_balancer_arn=load_balancer_arn,
                                       drop_invalid_header_fields=drop_invalid_header_fields, access_logs=access_logs)
+        return None
